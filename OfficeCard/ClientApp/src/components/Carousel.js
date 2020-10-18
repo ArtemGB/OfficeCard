7790f21d99474ea9ebea060beb03c4c94c7933b6
ArtemGB <gmadrich@gmail.com>
2020-10-18 03:05:42 +0300

    Правки по тексту и оформлению.

--- OfficeCard/ClientApp/src/components/Carousel.js
+++ OfficeCard/ClientApp/src/components/Carousel.js
@@ -1,31 +1,44 @@
 ﻿import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import './Carousel.css'
 
 export class DemoCarousel extends Component {
     static displayName = DemoCarousel.name;
     render() {
         return (
-            <Carousel width = "70%" height = "70%">
+            <Carousel width="70%" height="70%">
                 <div>
                     <img src="/Images/1Street.jpeg" />
-                    <p className="legend">Вид с улицы</p>
+                    <p className="legend CarouselLabel">Вид с улицы</p>
                 </div>
                 <div>
                     <img src="/Images/2StretDoor.jpeg" />
-                    <p className="legend">Legend 2</p>
+                    <p className="legend CarouselLabel">Вход в здание</p>
                 </div>
+                {/* <div>
+                    <img src="/Images/2BehindTheStreetDoor.jpeg" />
+                    <p className="legend CarouselLabel">Лестница на этаж</p>
+                </div> */}
+                {/* <div>
+                    <img src="/Images/3OnLevelDoor.jpeg" />
+                    <p className="legend CarouselLabel">Дверь на этаже</p>
+                </div> */}
                 <div>
                     <img src="/Images/3InsideRoom.jpeg" />
-                    <p className="legend">Legend 3</p>
+                    <p className="legend CarouselLabel">Основное помещение</p>
                 </div>
                 <div>
                     <img src="/Images/3InsideRoom1.jpeg" />
-                    <p className="legend">Legend 3</p>
+                    <p className="legend CarouselLabel">Основное помещение</p>
+                </div>
+                <div>
+                    <img src="/Images/4InsideRoom.jpeg" />
+                    <p className="legend CarouselLabel">Основное помещение</p>
                 </div>
                 <div>
                     <img src="/Images/3InsideRoom2.jpeg" />
-                    <p className="legend">Legend 3</p>
+                    <p className="legend CarouselLabel">Малое помещение</p>
                 </div>
             </Carousel>
         );
--- OfficeCard/ClientApp/src/components/Layout.js
+++ OfficeCard/ClientApp/src/components/Layout.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import './Layout.css';
 
 export class Layout extends Component {
   static displayName = Layout.name;
 
   render () {
     return (
-      <div>
-        <NavMenu />
+        <div class="mainLayout">
         <Container>
           {this.props.children}
         </Container>
