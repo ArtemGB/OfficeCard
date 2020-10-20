5b0129d8e5754ea207af3403582b7a7406d4d561
ArtemGB <gmadrich@gmail.com>
2020-10-21 02:22:38 +0300

    Новые изображения, описание кухни.

--- OfficeCard/ClientApp/src/components/ImagesCarousel.js
+++ OfficeCard/ClientApp/src/components/ImagesCarousel.js
@@ -29,6 +29,34 @@ const images = [
     {
         original: '/Images/3InsideRoom2.jpeg',
         thumbnail: '/Images/3InsideRoom2.jpeg',
+    },
+    {
+        original: '/Images/4InsideRoom.jpeg',
+        thumbnail: '/Images/4InsideRoom.jpeg',
+    },
+    {
+        original: '/Images/5Toulet.jpeg',
+        thumbnail: '/Images/5Toulet.jpeg',
+    },
+    {
+        original: '/Images/6InsRoom.jpg',
+        thumbnail: '/Images/6InsRoom.jpg',
+    },
+    {
+        original: '/Images/7insRoom.jpg',
+        thumbnail: '/Images/7insRoom.jpg',
+    },
+    {
+        original: '/Images/7insRoom.jpg',
+        thumbnail: '/Images/7insRoom.jpg',
+    },
+    {
+        original: '/Images/9InsRoom2.jpeg',
+        thumbnail: '/Images/9InsRoom2.jpeg',
+    },
+    {
+        original: '/Images/10InsRoom.jpeg',
+        thumbnail: '/Images/10InsRoom.jpeg',
     }
 ];
 
